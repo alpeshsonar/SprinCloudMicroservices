1fbb2633ff6ff265a87ca5ba535f9748593cc488
alpesh sonar <alpeshs2@AlpeshS2-MSD1.mumbai1.corp.citiustech.com>
2016-12-02 15:17:31 +0530

    Cross Filter

--- LoadBalancedAPI/src/main/java/com/rpicloud/LoadBalancedApplication.java
+++ LoadBalancedAPI/src/main/java/com/rpicloud/LoadBalancedApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.rpicloud.filters.pre.PreFilter;
 
@@ -23,5 +26,24 @@ public class LoadBalancedApplication
 	public PreFilter preFilter()
 	{
 		return new PreFilter();
+	}
+
+	@Bean
+	public CorsFilter corsFilter()
+	{
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("HEAD");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("DELETE");
+		config.addAllowedMethod("PATCH");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 }
